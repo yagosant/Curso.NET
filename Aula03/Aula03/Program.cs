b8490804e5c24871fc5d2b32d8afc60396a88ec9
Yago Santos <yagojf27@gmail.com>
2020-08-28 17:31:24 -0300

    fim curso 1

---
+++ Aula03/Aula03/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int ano = 2020;
+            int nasc = 1793;
+            int idade = ano - nasc;
+            if(idade < 18)
+            {
+                Console.WriteLine("Não pode votar");
+            }else if (idade >=18 && idade <=70)
+            {
+                Console.WriteLine("Pode votar de boa");
+            }
+            else
+            {
+                Console.WriteLine("Não é mais obrigatorio");
+            }
+            {
+
+                int mes = 10;
+
+                switch (mes)
+                {
+                    case 1:
+                        Console.WriteLine("O mês é Janeiro");
+                        break;
+                    case 2:
+                        Console.WriteLine("O mês é Fevereiro");
+                        break;
+                    case 3:
+                        Console.WriteLine("O mês é Março");
+                        break;
+                    case 4:
+                        Console.WriteLine("O mês é Abril");
+                        break;
+                    case 5:
+                        Console.WriteLine("O mês é Maio");
+                        break;
+                    case 6:
+                        Console.WriteLine("O mês é Junho");
+                        break;
+                    case 7:
+                        Console.WriteLine("O mês é Julho");
+                        break;
+                    case 8:
+                        Console.WriteLine("O mês é Agosto");
+                        break;
+                    case 9:
+                        Console.WriteLine("O mês é Setembro");
+                        break;
+                    case 10:
+                        Console.WriteLine("O mês é Outubro");
+                        break;
+                    case 11:
+                        Console.WriteLine("O mês é Novembro");
+                        break;
+                    case 12:
+                        Console.WriteLine("O mês é Dezembro");
+                        break;
+                    default:
+                        Console.WriteLine("Mês inválido");
+                        break;
+                }
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
--- aula02/Aula02/Aula02/Program.cs
+++ aula02/Aula02/Aula02/Program.cs
@@ -11,7 +11,26 @@ namespace Aula02
         static void Main(string[] args)
         {
             Console.WriteLine("Aula 02");
+
+            String nome = "Yago santos";
+            int ano = 2020;
+            int nasc = 1993;
+            int idade;
+
+            idade = ano - nasc;
+            Console.WriteLine("O " + nome + "Tem: " + idade + "anos.");
+
+            double peso = 4.0;
+            int quantidade = 10;
+            System.Console.WriteLine(peso * quantidade);
+
+            int a= 15 , b= 2;
+            System.Console.WriteLine(a / b);
+
             Console.ReadLine();
+            
+
+
         }
     }
 }
